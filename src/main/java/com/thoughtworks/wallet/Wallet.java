ee0b55fe88967ee6d9ea9e6747ef50f267086d54
Sai Preetham Dupalli <saipreethamdupalli@Sais-MacBook-Pro.local>
2022-04-04 19:09:59 +0530

    [Christeena | Sai Preetham] Add amount to wallet

--- src/main/java/com/thoughtworks/wallet/Wallet.java
+++ src/main/java/com/thoughtworks/wallet/Wallet.java
@@ -1,20 +1,22 @@
 package com.thoughtworks.wallet;
 
+import com.thoughtworks.wallet.exceptions.NegativeValueAmountException;
+
 public class Wallet {
-    private double currentBalance;
+    private Money balance;
 
 
-    public Wallet(double balance) {
-        this.currentBalance = balance;
-
-    }
-
-    public void credit(double amount) {
-        currentBalance += amount;
+    public Wallet() throws NegativeValueAmountException {
+        balance = new Money(0, Currency.RUPEE);
 
     }
 
-    public double balance() {
-        return currentBalance;
+    public void add(Money money) throws NegativeValueAmountException {
+        this.balance = this.balance.add(money);
+
+    }
+
+    public Money balance() {
+        return balance;
     }
 }
--- src/test/java/com/thoughtworks/wallet/WalletTest.java
+++ src/test/java/com/thoughtworks/wallet/WalletTest.java
@@ -1,21 +1,35 @@
 package com.thoughtworks.wallet;
 
+import com.thoughtworks.wallet.exceptions.NegativeValueAmountException;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class WalletTest {
     @Test
-    void shouldCreditAmountWhenItIsAddedToTheWallet() {
-        Wallet wallet = new Wallet(10.0);
+    void shouldAddAmountWhenItIsAddedToTheWallet() throws NegativeValueAmountException {
+        Money tenRupee = new Money(10, Currency.RUPEE);
+        Money anotherTenMoney = new Money(10, Currency.RUPEE);
+        Wallet wallet = new Wallet();
 
-        wallet.credit(10.0);
-        wallet.credit(5.0);
+        wallet.add(tenRupee);
 
-        double balance = wallet.balance();
-        assertThat(balance, is(equalTo(25.0)));
+        assertEquals(wallet.balance(), anotherTenMoney);
+    }
+
+    @Test
+    void shouldAddOneDollarAndOneRupeeToTheWallet() throws NegativeValueAmountException {
+        Money oneRupee = Money.createRupee(1);
+        Money oneDollar = Money.createDollar(1);
+        Wallet wallet = new Wallet();
+
+        Money seventySixRupees = new Money(76, Currency.RUPEE);
+
+        wallet.add(oneRupee);
+        wallet.add(oneDollar);
+
+        assertEquals(wallet.balance(), seventySixRupees);
+
     }
 }
